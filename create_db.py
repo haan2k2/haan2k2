60a0c8646fa48f82b0f258d1966da3cdca0f6cd5
Pham Hung Anh <101194740+haan2k2@users.noreply.github.com>
2022-04-28 23:09:24 +0700

    Delete create_db.py

--- create_db.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-def create_db():
-    con=sqlite3.connect(database=r'ism.db')
-    cur=con.cursor()
-    cur.execute("CREATE TABLE users(uid INTEGER PRIMARY KEY AUTOINCREMENT,name text,email text,gender text,phone text,dob text,dor text,pass text,utype text,address text,fees text)")
-    con.commit()
-
-create_db()
